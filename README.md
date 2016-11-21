d46d7d278791a2eab85da01c9e824456e479af43
Jose Felix Gomez <jose.gomez@koombea.com>
2016-11-21 16:24:09 -0500

    Adding base change endpoint. Refactoring the currency conversion and image generation code.

--- README.md
+++ README.md
@@ -5,8 +5,20 @@ the current exchange rate selected as an image.
 
 ## Usage
 
-Simply visit `/:Currency` to receive a PNG image with the data. Example:
+There are two endpoints currently:
+
+*Get currency exchange with USD base*: Simply visit `/:Currency` to receive a PNG image with the data. Example:
 
 ```
 GET /COP - 200 OK # returns a image with the value.
 ```
+
+*Get currency exchange with other base*: You can change the base
+currency used to perform the conversion using `/:base/to/:currency`.
+Example:
+
+```
+GET /EUR/to/COP - 200 OK # returns a image with the value.
+```
+
+Returns the exchange rate for 1 EUR to COP.
--- app.rb
+++ app.rb
@@ -5,20 +5,51 @@ require 'open-uri'
 
 APP_ID = ENV['APP_ID']
 
+get '/' do
+  return 200
+end
+
+get '/favicon.*' do
+  return 200
+end
+
 get '/:currency' do
   content_type 'image/png'
 
   currency = params['currency']
-  r = open("https://openexchangerates.org/api/latest.json?app_id=#{APP_ID}").read
-  value = JSON.parse(r)["rates"][currency].round(3)
+  value = convert(currency)
 
+  return text_to_img("#{value} #{currency}")
+end
+
+get '/:currency/to/:base' do
+  content_type 'image/png'
+
+  currency = params['currency']
+  base = params['base']
+  p [currency, base]
+  value = convert(currency, base)
+
+  return text_to_img("#{value} #{currency}")
+
+end
+
+def text_to_img(text)
   canvas = Magick::Image.new(420, 100){self.background_color = 'yellow'}
   canvas.format = "png"
   gc = Magick::Draw.new
   gc.pointsize(50)
-  gc.text(30,70, "#{value} #{currency}".center(14))
+  gc.text(30,70, text.center(14))
 
   gc.draw(canvas)
 
   canvas.to_blob
+end
+
+def convert(currency, base="USD")
+  r = open("https://openexchangerates.org/api/latest.json?app_id=#{APP_ID}").read
+  values = JSON.parse(r)["rates"]
+  value = (values[currency]/values[base]).round(3)
+
+  return value
 end
